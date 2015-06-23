6064413bbf2f7db819f0f64fa4dee0ead5f9b9d4
Jaquelinne <amorim.jaquelinne@gmail.com>
2015-06-23 09:48:23 -0300

    Classe Efeitos/ contém os efeitos Classe Imagem / get e set das imagens

---
+++ Projeto/src/modelo/Efeitos.java
@@ -0,0 +1,36 @@
+package modelo;
+
+import java.awt.image.BufferedImage;
+
+public class Efeitos {
+	
+	public Efeitos(){
+		
+	}
+	
+	public BufferedImage Negativo (BufferedImage imagem){
+		//Criar uma imagem de Buffer para receber manipulações
+		
+		BufferedImage imagem_negativa = new BufferedImage(imagem.getWidth(), imagem.getHeight(), BufferedImage.TYPE_INT_RGB);
+		
+		for (int y = 0; y < imagem.getHeight(); y++) {
+			for (int x = 0; x < imagem.getWidth(); x++) {
+				// pega o nivel de cor de um pixel
+				// retornará o inverso do valor do pixel
+				int nivel_cinza = 255 - (int) imagem.getRGB(y, x);
+				// chama função gera nivel de cinza que faz deslocamento de bits
+				nivel_cinza = geraNivelCinza(nivel_cinza);
+				
+				imagem_negativa.setRGB(y, x, nivel_cinza);
+			}
+			
+		}
+		return imagem_negativa;
+	}
+
+	private int geraNivelCinza(int valor) {
+		// TODO Auto-generated method stub
+		return valor | valor << 8 | valor << 16; // Faz deslocamento de bits
+	}
+
+}
---
+++ Projeto/src/modelo/Imagem.java
@@ -0,0 +1,23 @@
+package modelo;
+
+import java.awt.image.BufferedImage;
+
+public class Imagem {
+
+	BufferedImage im1;
+	BufferedImage im2;
+	
+	public BufferedImage getIm1() {
+		return im1;
+	}
+	public void setIm1(BufferedImage im1) {
+		this.im1 = im1;
+	}
+	public BufferedImage getIm2() {
+		return im2;
+	}
+	public void setIm2(BufferedImage im2) {
+		this.im2 = im2;
+	}
+	
+}
